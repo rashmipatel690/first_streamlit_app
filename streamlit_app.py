d3587070ca0d7b6eee19c66cd93a2d9c6983cc28
rashmipatel690 <141769331+rashmipatel690@users.noreply.github.com>
2023-08-08 16:01:55 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,6 @@
 import streamlit
 streamlit.title('My parents New Healthy Diner')
-streamlit.title('Breakfast Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Oats & Almond Milk')
+streamlit.text('Sprouts')
+streamlit.text('Apple smoothie, Pancake & Kale')
