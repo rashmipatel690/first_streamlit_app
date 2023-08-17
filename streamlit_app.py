7778adcc68b8311314fbf3c3f726153f0b6f01ce
rashmipatel690 <141769331+rashmipatel690@users.noreply.github.com>
2023-08-17 14:10:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-# Display the table on the pageh7j
+# Display the table on the pageh7
 streamlit.dataframe(fruits_to_show)
 
 #new section to display fruityvice api 
